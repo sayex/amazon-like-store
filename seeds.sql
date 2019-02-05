e2a870135c1897f7a23ced0dba06db6d24be16a0
sayex <elsayer@msn.com>
2019-02-05 01:03:08 -0700

    updated seed

--- seeds.sql
+++ seeds.sql
@@ -69,7 +69,7 @@ INSERT INTO `products` VALUES (1,'Polo Shirt',1,24.99,205);
 INSERT INTO `products` VALUES (2,'Polo Shirt',2,24.99,2);
 INSERT INTO `products` VALUES (3,'Tickle Me Elmo',3,99.99,5);
 INSERT INTO `products` VALUES (4,'Head Light',4,149.99,20);
-INSERT INTO `products` VALUES (5,'Dress Pants',1,110.99,NULL);
+INSERT INTO `products` VALUES (5,'Dress Pants',1,110.99,299);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
