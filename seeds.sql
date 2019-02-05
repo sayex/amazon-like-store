786922f1f45cd8366bfeea7c61378396eb99505c
sayex <elsayer@msn.com>
2019-02-05 02:01:33 -0700

    merged sql file

--- seeds.sql
+++ seeds.sql
@@ -1,33 +1,21 @@
--- MySQL dump 10.13  Distrib 8.0.14, for macos10.14 (x86_64)
---
--- Host: 127.0.0.1    Database: bamazon
--- ------------------------------------------------------
--- Server version	8.0.14
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `departments`
---
-
-DROP TABLE IF EXISTS `departments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
+DROP Database IF EXISTS bamazon;
+CREATE DATABASE `bamazon` ;
+Use bamazon;
 CREATE TABLE `departments` (
   `iddepartments` int(11) NOT NULL AUTO_INCREMENT,
   `department_name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`iddepartments`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
+
+CREATE TABLE `products` (
+  `idproducts` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(45) DEFAULT NULL,
+  `department_id` int(11) NOT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `stock_quantity` decimal(10,0) DEFAULT NULL,
+  PRIMARY KEY (`idproducts`)
+);
+
 
 --
 -- Dumping data for table `departments`
@@ -46,19 +34,6 @@ UNLOCK TABLES;
 -- Table structure for table `products`
 --
 
-DROP TABLE IF EXISTS `products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `products` (
-  `idproducts` int(11) NOT NULL AUTO_INCREMENT,
-  `product_name` varchar(45) DEFAULT NULL,
-  `department_id` int(11) NOT NULL,
-  `price` decimal(10,2) DEFAULT NULL,
-  `stock_quantity` decimal(10,0) DEFAULT NULL,
-  PRIMARY KEY (`idproducts`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `products`
 --
@@ -73,17 +48,3 @@ INSERT INTO `products` VALUES (5,'Dress Pants',1,110.99,299);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Dumping routines for database 'bamazon'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-02-05  0:38:51
