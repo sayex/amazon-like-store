dbcaed53511bdeaba06fbb19087d92d7802e5b1a
sayex <elsayer@msn.com>
2019-02-06 14:08:14 -0700

    .

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,85 +2,88 @@ var inquirer = require("inquirer");
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
+  host: "localhost",
 
-    // Your port; if not 3306
-    port: 3306,
+  // Your port; if not 3306
+  port: 3306,
 
-    // Your username
-    user: "root",
+  // Your username
+  user: "root",
 
-    // Your password
-    password: "newpassword",
-    database: "bamazon"
+  // Your password
+  password: "newpassword",
+  database: "bamazon"
 });
 
-
 function readProducts(callback) {
-    connection.query("SELECT * from product_view", function (err, res) {
-        if (err) throw err;
-        callback(res);
-
-    });
+  connection.query("SELECT * from product_view", function(err, res) {
+    if (err) throw err;
+    callback(res);
+  });
 }
 
 function updatepurchase(newQty, id) {
-
-    connection.query("UPDATE products SET stock_quantity =? WHERE idproducts =?", [newQty, id], function (err, res) {
-        if (err) throw err;
-
-    });
-
+  connection.query("UPDATE products SET stock_quantity =? WHERE idproducts =?", [newQty, id], function(err, res) {
+    if (err) throw err;
+  });
 }
 
 function customerSelection() {
-    readProducts(function (res) {
-        var resArr = [];
+  readProducts(function(res) {
+    var resArr = [];
 
-        for (var i = 0; i < res.length; i++) {
-            resArr.push(res[i].idproducts + "  " + res[i].product_name + " || Dept: " + res[i].department_name + " || Product Price: " + res[i].price + " || Stock Qty: " + res[i].stock_quantity);
-
+    for (var i = 0; i < res.length; i++) {
+      resArr.push(
+        res[i].idproducts +
+          "  " +
+          res[i].product_name +
+          " || Dept: " +
+          res[i].department_name +
+          " || Product Price: " +
+          res[i].price +
+          " || Stock Qty: " +
+          res[i].stock_quantity
+      );
+    }
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "product_name",
+          message: "What would you like to buy?",
+          choices: resArr
+        },
+        {
+          type: "input",
+          name: "purchaseQty",
+          message: "How many would you like to buy?"
         }
-        inquirer
-            .prompt([
-                {
-                    type: 'list',
-                    name: 'product_name',
-                    message: 'What would you like to buy?',
-                    choices: resArr
-                },
-                {
-                    type: 'input',
-                    name: 'purchaseQty',
-                    message: 'How many would you like to buy?',
-                },
-            ])
-            .then(answers => {
-                var productNameId = answers.product_name.split(" ")
-                productNameId = productNameId[0]
-                var productNameIdIndex = productNameId[0] - 1
-                var userQty = parseInt(answers.purchaseQty)
-                var databaseQty = res[productNameIdIndex].stock_quantity
-                var databasePrice = res[productNameIdIndex].price
-                if (userQty > databaseQty) {
-                    console.log("-----------------------");
-                    console.log();
-                    console.log("Not enough in inventory");
-                    console.log("-----------------------");
-                    console.log();
-                } else if (userQty < databaseQty) {
-                    var totalPrucahse = userQty * databasePrice;
-                    var newQty = databaseQty - userQty;
-                    updatepurchase(newQty, productNameId)
-                    console.log("-----------------------");
-                    console.log();
-                    console.log("Enough is in Inventory for purchase." + " || Purchase Price: $" + totalPrucahse);
-                    console.log("-----------------------");
-                    console.log();
-
-                }
-                customerSelection()
-            });
-    });
-};
-customerSelection()
+      ])
+      .then(answers => {
+        var productNameId = answers.product_name.split(" ");
+        productNameId = productNameId[0];
+        var productNameIdIndex = productNameId[0] - 1;
+        var userQty = parseInt(answers.purchaseQty);
+        var databaseQty = res[productNameIdIndex].stock_quantity;
+        var databasePrice = res[productNameIdIndex].price;
+        if (userQty > databaseQty) {
+          console.log("-----------------------");
+          console.log();
+          console.log("Not enough in inventory");
+          console.log("-----------------------");
+          console.log();
+        } else if (userQty < databaseQty) {
+          var totalPrucahse = userQty * databasePrice;
+          var newQty = databaseQty - userQty;
+          updatepurchase(newQty, productNameId);
+          console.log("-----------------------");
+          console.log();
+          console.log("Enough is in Inventory for purchase." + " || Purchase Price: $" + totalPrucahse);
+          console.log("-----------------------");
+          console.log();
+        }
+        customerSelection();
+      });
+  });
+}
+customerSelection();
